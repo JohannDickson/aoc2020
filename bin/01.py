bb9fbb8d98cc462349e4b6b34b2c8f8b7084794d
Johann Dickson <johann.dickson@gmail.com>
2020-12-01 10:00:06 +0000

    Add working test for pt1.1

---
+++ bin/01.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import os
+
+inputfile = "../tests/01.txt"
+
+with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
+    numbers = [int(x) for x in f.readlines()]
+
+for i in range(0, len(numbers)):
+    n = numbers[i]
+    for j in range(0, len(numbers[i:])):
+        m = numbers[i+j]
+        if (n+m) == 2020:
+            print(n*m)
