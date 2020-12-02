8b082df4c33cd6ec28f41e2ae0d18639d1e9253f
Johann Dickson <johann.dickson@gmail.com>
2020-12-02 10:35:37 +0000

    Split day 1 into functions

--- bin/01.py
+++ bin/01.py
@@ -7,13 +7,27 @@ inputfile = "../tests/01.txt"
 with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
     numbers = [int(x) for x in f.readlines()]
 
-for i in range(0, len(numbers)):
-    n = numbers[i]
-    for j in range(0, len(numbers[i:])):
-        m = numbers[i+j]
-        if (n+m) == 2020:
-            print("Part 1:", n*m)
-        for k in range(0, len(numbers[i+j:])):
-            o = numbers[i+j+k]
-            if (n+m+o) == 2020:
-                print("Part 2:", n*m*o)
+
+def part1(numbers):
+    for i in range(0, len(numbers)):
+        n = numbers[i]
+        for j in range(0, len(numbers[i:])):
+            m = numbers[i+j]
+            if (n+m) == 2020:
+                return n*m
+
+
+def part2(numbers):
+    for i in range(0, len(numbers)):
+        n = numbers[i]
+        for j in range(0, len(numbers[i:])):
+            m = numbers[i+j]
+            for k in range(0, len(numbers[i+j:])):
+                o = numbers[i+j+k]
+                if (n+m+o) == 2020:
+                    return n*m*o
+
+
+if __name__ == '__main__':
+    print("Part 1:", part1(numbers))
+    print("Part 2:", part2(numbers))
