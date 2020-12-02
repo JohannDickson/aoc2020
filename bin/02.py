8631542c88a23cdd35c7959c69c5626cbfc4c444
Johann Dickson <johann.dickson@gmail.com>
2020-12-02 10:26:42 +0000

    Add pt2.2

--- bin/02.py
+++ bin/02.py
@@ -22,4 +22,20 @@ def part1(entries):
     return validPasswords
 
 
+def part2(entries):
+    validPasswords = 0
+
+    for i in entries:
+        limits, letter, password = i.split(" ")
+        lower, upper = [int(x) for x in limits.split("-")]
+        letter = letter[:-1]
+
+        if (password[lower-1] == letter and password[upper-1] != letter) \
+        or (password[lower-1] != letter and password[upper-1] == letter):
+            validPasswords += 1
+
+    return validPasswords
+
+
 print("Part 1:", part1(entries))
+print("Part 2:", part2(entries))
