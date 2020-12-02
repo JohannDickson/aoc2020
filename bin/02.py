7725ae4e323782c02be9097c28b9e1dd515ea602
Johann Dickson <johann.dickson@gmail.com>
2020-12-02 10:26:02 +0000

    Reformat pt2.1

--- bin/02.py
+++ bin/02.py
@@ -7,14 +7,19 @@ inputfile = "../tests/02.txt"
 with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
     entries = [x.strip() for x in f.readlines()]
 
-validPasswords = 0
 
-for i in entries:
-    limits, letter, password = i.split(" ")
-    lower, upper = limits.split("-")
-    letter = letter[:-1]
+def part1(entries):
+    validPasswords = 0
 
-    if int(lower) <= password.count(letter) <= int(upper):
-        validPasswords += 1
+    for i in entries:
+        limits, letter, password = i.split(" ")
+        lower, upper = limits.split("-")
+        letter = letter[:-1]
 
-print("Part 1:", validPasswords)
+        if int(lower) <= password.count(letter) <= int(upper):
+            validPasswords += 1
+
+    return validPasswords
+
+
+print("Part 1:", part1(entries))
