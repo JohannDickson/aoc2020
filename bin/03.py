467adfa929605aa5feb26b86292f3da733e451a9
Johann Dickson <johann.dickson@gmail.com>
2020-12-03 13:14:38 +0000

    Update outputs

--- bin/03.py
+++ bin/03.py
@@ -12,9 +12,9 @@ with open( os.path.join(os.path.dirname(__file__), testInput) ) as f:
 
 def printGrid(gridToPrint, display):
     if display:
+        out = '\n'.join([''.join(y) for y in gridToPrint])
         os.system('clear')
-        for y in gridToPrint:
-            print(''.join(y))
+        print(out)
         sleep(0.2)
 
 
@@ -47,18 +47,20 @@ def part1(treeGrid, slope_x, slope_y, display=False):
     return treeCounter
 
 
-def part2(treeGrid, slopes):
+def part2(treeGrid, slopes, display=False):
     results = []
     for slope in slopes:
-        results.append(part1(deepcopy(treeGrid), slope[0], slope[1]))
+        results.append(part1(deepcopy(treeGrid), slope[0], slope[1], display))
 
     result = 1
     for i in results:
         result = result * i
-    return result, results
+    return result
 
 
 if __name__ == '__main__':
+    p1 = part1(testInput, 3, 1, False)
+    p2 = part2(testInput, [(1,1), (3,1), (5,1), (7,1), (1,2)])
     print("Tests:")
-    print("Part 1:", part1(deepcopy(testInput), 3, 1, False))
-    print("Part 2:", part2(deepcopy(testInput), [(1,1), (3,1), (5,1), (7,1), (1,2)]))
+    print("Part 1:", p1)
+    print("Part 2:", p2)
