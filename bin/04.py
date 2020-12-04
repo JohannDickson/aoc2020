ac1ea76d61f1d45ef96b475a9744eb0da05746b5
Johann Dickson <johann.dickson@gmail.com>
2020-12-04 12:37:59 +0000

    First draft pt4.2

--- bin/04.py
+++ bin/04.py
@@ -3,7 +3,9 @@
 import os
 
 
-testfile = "../tests/04.txt"
+# testfile = "../tests/04.txt"
+testfile = "../tests/04_2_1.txt"
+# testfile = "../tests/04_2_2.txt"
 with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
     testInput = [x.replace('\n', ' ').strip() for x in f.read().split('\n\n')]
 
@@ -30,15 +32,74 @@ def validateKeys(passport):
     return all([ef in passport.keys() for ef in expectedFields])
 
 
+def validateValues(p):
+    if  1920 <= int(p['byr']) <= 2002:
+        pass
+    else:
+        print("INVALID byr", p['byr'])
+        return False
+
+    if 2010 <= int(p['iyr']) <= 2020:
+        pass
+    else:
+        print("INVALID iyr", p['iyr'])
+        return False
+
+    if 2020 <= int(p['eyr']) <= 2030:
+        pass
+    else:
+        print("INVALID eyr", p['eyr'])
+        return False
+
+    if (p['hgt'][-2:] == 'cm' and 150 <= int(p['hgt'][:-2]) <= 193) \
+    or p['hgt'][-2:] == 'in' and 59 <= int(p['hgt'][:-2]) <= 76:
+        pass
+    else:
+        print("INVALID hgt", p['hgt'])
+        return False
+
+    if p['hcl'][0] == '#' and len(p['hcl']) == 7:
+        pass
+    else:
+        print("INVALID hcl", p['hcl'])
+        return False
+
+    if p['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+        pass
+    else:
+        print("INVALID ecl", p['ecl'])
+        return False
+
+    if p['pid']:
+        pass
+    else:
+        print("INVALID pid", p['pid'])
+        return False
+
+    print('VALID')
+    return True
+
+
 def part1(passports):
     passports = [parsePassport(p) for p in passports]
     validPassports = [validateKeys(p) for p in passports].count(True)
     return validPassports
 
 
+def part2(passports):
+    passports = [parsePassport(p) for p in passports]
+    validPassports = 0
+    for passport in passports:
+        if validateKeys(passport) and validateValues(passport):
+            validPassports += 1
+
+    return validPassports
+
+
 if __name__ == '__main__':
     print("Tests:")
     print("Part 1:", part1(testInput))
+    print("Part 2:", part2(testInput))
 
     print()
 
