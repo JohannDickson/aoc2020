e03336e2466957fb9148c2011c2c452d61485e4f
Johann Dickson <johann.dickson@gmail.com>
2020-12-05 09:47:03 +0000

    Add code solution for pt5.1

--- bin/05.py
+++ bin/05.py
@@ -12,10 +12,17 @@ with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
     myInput = [x.strip() for x in f.readlines()]
 
 
+def getSeatNumber(boardingPass):
+    for r in ['F0', 'B1', 'L0', 'R1']:
+        boardingPass = boardingPass.replace(r[0], r[1])
+    row = int(boardingPass[:7], 2)
+    col = int(boardingPass[7:], 2)
+    return row*8 + col
+
+
 def part1(boardingPasses):
-    # Did this manually,
-    # Binary representation, F/L = 0, B/R = 1
-    return boardingPasses
+    seatNumbers = [getSeatNumber(p) for p in boardingPasses]
+    return max(seatNumbers)
 
 
 if __name__ == '__main__':
