9f73aca923c61a12e6eb7ac10a85e3e34bb4903c
Johann Dickson <johann.dickson@gmail.com>
2020-12-06 09:57:45 +0000

    Add pt6.2

--- bin/06.py
+++ bin/06.py
@@ -5,23 +5,36 @@ import os
 
 testfile = "../tests/06.txt"
 with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
-    testInput = [x.replace('\n', '').strip() for x in f.read().split('\n\n')]
+    testInput = [x.strip() for x in f.read().split('\n\n')]
 
 inputfile = "../input/06.txt"
 with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
-    myInput = [x.replace('\n', '').strip() for x in f.read().split('\n\n')]
+    myInput = [x.strip() for x in f.read().split('\n\n')]
 
 
 def part1(answers):
-    uniqueAnswers = [set(x) for x in answers]
+    uniqueAnswers = [set(x.replace('\n', '')) for x in answers]
     return sum([len(x) for x in uniqueAnswers])
+
+
+def part2(answers):
+    commonResponse = 0
+    for group in answers:
+        allUniqueAnswers = set(group.replace('\n', ''))
+        personAnswers = group.split('\n')
+        for r in allUniqueAnswers:
+            if all([r in a for a in personAnswers]):
+                commonResponse += 1
+    return commonResponse
 
 
 if __name__ == '__main__':
     print("Tests:")
     print("Part 1:", part1(testInput))
+    print("Part 2:", part2(testInput))
 
     print()
 
     print("Mine:")
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
