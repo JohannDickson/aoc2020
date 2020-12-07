6da37458c3e5ca9230cc8a2311e1e7efc84b771a
Johann Dickson <johann.dickson@gmail.com>
2020-12-07 09:38:36 +0000

    Add pt7.1 draft parsing

---
+++ bin/07.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import os
+import re
+
+
+testfile = "../tests/07.txt"
+with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
+    testInput = [x.strip() for x in f.readlines()]
+
+inputfile = "../input/07.txt"
+with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
+    myInput = [x.strip() for x in f.readlines()]
+
+
+def getBagContents(bag):
+    currentBag, contents = bag.split(' bags contain ')
+
+    noBag = re.compile("no other bags\.$")
+    bagFilter = re.compile("((\d+) ([a-z ]+) bag)")
+
+    if noBag.match(contents):
+        return {currentBag: ''}
+
+    whatsInTheBag = {}
+    for b in bagFilter.findall(contents):
+        whatsInTheBag[b[2]] = b[1]
+    return {currentBag: whatsInTheBag}
+
+
+def part1(bags):
+    allBags = {}
+    for bag in bags:
+        allBags.update(getBagContents(bag))
+    return
+
+
+if __name__ == '__main__':
+    print("Tests:")
+    print("Part 1:", part1(testInput))
+
+    print()
+
+    print("Mine:")
+    # print("Part 1:", part1(myInput))
