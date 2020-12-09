692cf2ffde0bc4c902a6bb6ee8260c008cdc2265
Johann Dickson <johann.dickson@gmail.com>
2020-12-09 09:46:39 +0000

    Add pt9.2

--- bin/09.py
+++ bin/09.py
@@ -27,11 +27,29 @@ def part1(xmas, lookback):
     return False
 
 
+def part2(xmas, lookback):
+    invalidNumber = part1(xmas, lookback)
+    for i in range(len(xmas)):
+        contiguousSum = 0
+        for x in range(i, len(xmas)):
+            contiguousSum += xmas[x]
+            if contiguousSum > invalidNumber:
+                continue
+            elif contiguousSum == invalidNumber:
+                contiguous = xmas[i:x]
+                lower = min(contiguous)
+                upper = max(contiguous)
+                return lower+upper
+    return False
+
+
 if __name__ == '__main__':
     print("Tests:")
     print("Part 1:", part1(testInput, 5))
+    print("Part 2:", part2(testInput, 5))
 
     print()
 
     print("Mine:")
     print("Part 1:", part1(myInput, 25))
+    print("Part 2:", part2(myInput, 25))
