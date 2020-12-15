16b125947fc59ed52813f6d14d204db3f59e6ec9
Johann Dickson <johann.dickson@gmail.com>
2020-12-16 09:11:52 +1300

    Add memory-hungry pt14.2

--- bin/14.py
+++ bin/14.py
@@ -4,7 +4,7 @@ import os
 import re
 
 
-testfile = "../tests/14.txt"
+testfile = "../tests/14_2.txt"
 with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
     testInput = [x.strip() for x in f.readlines()]
 
@@ -41,11 +41,49 @@ def part1(initialization):
     return sum(memory)
 
 
+def part2(initialization):
+    memAddresses = []
+    remem = re.compile('^mem\[(\d+)\] = (\d+)')
+    remask = re.compile('^mask = ([01X]+)')
+    currentMask = ''
+    memory = [0]
+
+    for x in initialization:
+        mask = remask.match(x)
+        mem = remem.match(x)
+        if mask:
+            currentMask = list(mask[1])
+        elif mem:
+            address = int(mem[1])
+            value = int(mem[2])
+
+            badd = list(format(address, '036b'))
+            addSpace = [''.join(badd[:currentMask.index('X')])]
+            for i in range(0, len(currentMask)):
+                if currentMask[i] == '1':
+                    badd[i] = currentMask[i]
+                if currentMask[i] == 'X':
+                    newAddresses = []
+                    for a in addSpace:
+                        newAddresses.extend([a+''.join(badd[len(a):i])+'0', a+''.join(badd[len(a):i])+'1'])
+                    addSpace = newAddresses
+
+            for a in addSpace:
+                a = int(a, 2)
+                if a >= len(memory):
+                    memory.extend([0]*(a - len(memAddresses)))
+                memory[a] = int(value)
+
+    return sum(memory)
+
+
 if __name__ == '__main__':
     print("Tests:")
-    print("Part 1:", part1(testInput))
+    # print("Part 1:", part1(testInput))
+    print("Part 2:", part2(testInput))
 
     print()
 
     print("Mine:")
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
