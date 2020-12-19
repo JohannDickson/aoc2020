a278a864e1a7031c553a5f16114a79a38a03f768
Johann Dickson <johann.dickson@gmail.com>
2020-12-19 22:04:53 +1300

    halfway

--- bin/19.py
+++ bin/19.py
@@ -5,7 +5,7 @@ import re
 from copy import deepcopy
 
 
-testfile = "../tests/19.txt"
+testfile = "../tests/19_2.txt"
 with open( os.path.join(os.path.dirname(__file__), testfile) ) as f:
     testInput = f.read().strip()
 
@@ -31,23 +31,31 @@ def part1(imageData):
     for l in letters:
         solved[l] = True
 
-    regregex = re.compile('[\(\)\|'+''.join(letters.values())+']')
+    regregex = re.compile('[\(\)\|\+'+''.join(letters.values())+']')
 
     while not all(solved.values()):
         for k in [x for x in solved.keys() if not solved[x]]:
             for j in range(0, len(rules[k])):
-                    for l in letters:
-                        if rules[k][j] == l:
-                            rules[k][j] = letters[l]
-                            break
-                        for a in [x for x in solved.keys() if solved[x]]:
-                            if rules[k][j] == a:
-                                rules[k][j] = rules[a]
-                                break
 
-                    if (all([regregex.match(x) for x in rules[k]])):
-                        rules[k] = '('+''.join(rules[k])+')'
-                        solved[k] = True
+                if rules[k][j] == k:
+                    if k == '8':
+                        rules[k][j] = '+'
+                    if k == '11':
+                        pass
+
+                for l in letters:
+                    if rules[k][j] == l:
+                        rules[k][j] = letters[l]
+                        break
+
+                    for a in [x for x in solved.keys() if solved[x]]:
+                        if rules[k][j] == a:
+                            rules[k][j] = rules[a]
+                            break
+
+                if (all([regregex.match(x) for x in rules[k]])):
+                    rules[k] = '('+''.join(rules[k])+')'
+                    solved[k] = True
 
     validMessages = []
     stringCheck = re.compile('^'+rules['0']+'$')
@@ -58,11 +66,19 @@ def part1(imageData):
     return len(validMessages)
 
 
+def part2(imageData):
+    imageData = imageData.replace("8: 42\n", "8: 42 | 42 8\n")
+    imageData = imageData.replace("11: 42 31\n", "11: 42 31 | 42 11 31\n")
+    return part1(imageData)
+
+
 if __name__ == '__main__':
     print("Tests:")
     print("Part 1:", part1(testInput))
+    print("Part 2:", part2(testInput))
 
     print()
 
     print("Mine:")
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
