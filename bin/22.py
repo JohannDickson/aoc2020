cfb26fca6928f22e089bc184f3d5c2ebd9a24a22
Johann Dickson <johann.dickson@gmail.com>
2020-12-22 21:31:46 +1300

    Add pt22.2

--- bin/22.py
+++ bin/22.py
@@ -12,6 +12,50 @@ with open( os.path.join(os.path.dirname(__file__), inputfile) ) as f:
     myInput = f.read().strip()
 
 
+class recursiveCombat():
+    def __init__(self, players):
+        self.players = players
+        self.previousRounds = []
+
+    def __repr__(self):
+        return '|'.join([','.join([str(x) for x in p]) for p in self.players])
+
+    def playGame(self):
+        while all([len(p) for p in self.players]):
+            # Tiebreaker
+            thisRound = str(self)
+            if thisRound in self.previousRounds:
+                winner = 0
+                break
+            else:
+                self.previousRounds.append(thisRound)
+
+            # Regular play
+            cardsDrawn = []
+            for p in range(0, len(self.players)):
+                cardsDrawn.append(self.players[p].pop(0))
+
+            # Recursive combat
+            if all([len(self.players[p]) >= cardsDrawn[p] for p in range(len(self.players))]):
+                subDeck = [self.players[p][:cardsDrawn[p]] for p in range(len(self.players))]
+                newGame = recursiveCombat(subDeck)
+                winner = newGame.playGame()
+
+            # Not enough cards, regular
+            else:
+                highest = -1
+                winner = None
+                for c in cardsDrawn:
+                    if c >highest:
+                        highest = c
+                        winner = cardsDrawn.index(c)
+
+            self.players[winner].append(cardsDrawn.pop(winner))
+            self.players[winner].append(cardsDrawn.pop())
+
+        return winner
+
+
 def playRound(players):
     cardsDrawn = []
     for p in range(0, len(players)):
@@ -48,11 +92,23 @@ def part1(cardDecks):
     return getWinnerScore(players)
 
 
+def part2(cardDecks):
+    players = cardDecks.split('\n\n')
+    for p in range(0, len(players)):
+        players[p] = [int(x) for x in players[p].split('\n')[1:]]
+    game = recursiveCombat(players)
+    game.playGame()
+
+    return getWinnerScore(game.players)
+
+
 if __name__ == '__main__':
     print("Tests:")
     print("Part 1:", part1(testInput))
+    print("Part 2:", part2(testInput))
 
     print()
 
     print("Mine:")
     print("Part 1:", part1(myInput))
+    print("Part 2:", part2(myInput))
